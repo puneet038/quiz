5dc527ece8e464d3f5cf2ba2a5bed6f2866e82dd
puneet038 <63274458+puneet038@users.noreply.github.com>
2021-05-09 19:36:31 +0530

    commit

---
+++ quiz.js
@@ -0,0 +1,128 @@
+const quizdb=
+[
+    {
+    question:"Q1. A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+
+a: "Encapsulation",
+b: "Polymorphism",
+c: "Abstraction",
+d:"Inheritance",
+ans: "ans1"
+    },
+    
+        {
+            question:"Q2. A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+        
+        a: "Encapsulation",
+        b: "Polymorphism",
+        c: "Abstraction",
+        d:"Inheritance",
+        ans: "ans1"
+            },
+            {
+                question:"Q3. A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+            
+            a: "Encapsulation",
+            b: "Polymorphism",
+            c: "Abstraction",
+            d:"Inheritance",
+            ans: "ans1"
+                },
+                {
+                    question:"Q4. A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+                
+                a: "Encapsulation",
+                b: "Polymorphism",
+                c: "Abstraction",
+                d:"Inheritance",
+                ans: "ans1"
+                    },
+
+                    {
+                        question:"Q5. A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+                    
+                    a: "Encapsulation",
+                    b: "Polymorphism",
+                    c: "Abstraction",
+                    d:"Inheritance",
+                    ans: "ans1"
+                        },{
+                            question:"Q6. A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+                        
+                        a: "Encapsulation",
+                        b: "Polymorphism",
+                        c: "Abstraction",
+                        d:"Inheritance",
+                        ans: "ans1"
+                            },{
+                                question:"Q7. A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+                            
+                            a: "Encapsulation",
+                            b: "Polymorphism",
+                            c: "Abstraction",
+                            d:"Inheritance",
+                            ans: "ans1"
+                                },
+
+                                {
+                                    question:"Q8. A process that involves recognizing and focusing on the important characteristics of a situation or object is known as:",
+                                
+                                a: "Encapsulation",
+                                b: "Polymorphism",
+                                c: "Abstraction",
+                                d:"Inheritance",
+                                ans: "ans1"
+                                    }
+                                ];
+                                const question =document.querySelector('.question');
+                                 const option1 =document.querySelector('#option1');
+                                const option2 =document.querySelector('#option2');
+                                const option3 =document.querySelector('#option3');
+                                const option4 =document.querySelector('#option4');
+                                const submit =document.querySelector('#submit');
+                                const answers=document.querySelectorAll('.answer');
+                                const showscore=document.querySelector('#showscore');
+                               let countquestion=0;
+                               let score=0;
+                                const uploadquestion=()=>{
+                                    const questionList=quizdb[countquestion];
+                                   question.innerText=questionList.question;
+                                    option1.innerText=questionList.a;
+                                    option2.innerText=questionList.b;
+                                    option3.innerText=questionList.c;
+                                    option4.innerText=questionList.d;
+
+                                }
+                                uploadquestion();
+                                const getcheckanswer =()=>
+                                {
+                                    let answer;
+                                    answers.forEach((currentelement) => {
+                                        if(currentelement.checked)
+                                        {
+                                       answer=currentelement.id;
+                                        }
+                                    });
+                                    return answer;
+
+                                }
+                                submit.addEventListener('click',() =>
+                                {
+                                    const checkAnswer=getcheckanswer();
+                                    console.log(checkAnswer);
+                                   if(checkAnswer == quizdb[countquestion].ans)
+                                    {
+                                       score++;
+                                    };
+                                    countquestion++;
+                                    if(countquestion<quizdb.length)
+                                    {
+                                        uploadquestion();
+                                    }
+                                   else{
+                                   var pro=(score/quizdb.length)*100;
+                                   console.log(pro+"%");
+                                   document.write(pro);
+                                   }
+
+                                });
